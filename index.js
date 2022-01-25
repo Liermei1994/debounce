a567cc5a9ad66d16d54b79006304a52e29f02c69
Liermei1994 <42959487+Liermei1994@users.noreply.github.com>
2022-01-25 12:03:00 +0800

    debounce function

---
+++ index.js
@@ -0,0 +1,26 @@
+function debounce(fn,delay,immediate = false,callBackFunction){
+    let timer = null
+    let isInvoce = false
+    const _debounce = function(...args){
+        return new Promise((resolve,reject) =>{
+            if(timer) clearTimeout(timer)
+            if(immediate && !isInvoce){
+                const result = fn.apply(this,args)
+                if(callBackFunction) callBackFunction(result)
+                resolve(result)
+                isInvoce = true
+            }else{
+                timer = setTimeout(() => {
+                    const result = fn.apply(this,args)
+                    if(callBackFunction) callBackFunction(result)
+                    resolve(result)
+                    timer = null 
+                    isInvoce = false
+                }, delay);
+            }
+        })
+        
+    }
+
+    return _debounce
+}
