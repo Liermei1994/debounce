e846d4e772d270a7ef33a867d6773717ebbf330d
Liermei1994 <42959487+Liermei1994@users.noreply.github.com>
2022-01-25 14:11:02 +0800

    cancel function

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-function debounce(fn,delay,immediate = false,callBackFunction){
+function debounce(fn, delay, immediate = false, callBackFunction) {
     let timer = null
     let isInvoce = false
-    const _debounce = function(...args){
-        return new Promise((resolve,reject) =>{
-            if(timer) clearTimeout(timer)
-            if(immediate && !isInvoce){
-                const result = fn.apply(this,args)
-                if(callBackFunction) callBackFunction(result)
+    const _debounce = function (...args) {
+        return new Promise((resolve, reject) => {
+            if (timer) clearTimeout(timer)
+            if (immediate && !isInvoce) {
+                const result = fn.apply(this, args)
+                if (callBackFunction) callBackFunction(result)
                 resolve(result)
                 isInvoce = true
-            }else{
+            } else {
                 timer = setTimeout(() => {
-                    const result = fn.apply(this,args)
-                    if(callBackFunction) callBackFunction(result)
+                    const result = fn.apply(this, args)
+                    if (callBackFunction) callBackFunction(result)
                     resolve(result)
-                    timer = null 
+                    timer = null
                     isInvoce = false
                 }, delay);
             }
         })
-        
+
+    }
+
+    _debounce.cancel = function () {
+        if (timer) clearTimeout(timer)
+        timer = null
+        isInvoce = false
     }
 
     return _debounce
